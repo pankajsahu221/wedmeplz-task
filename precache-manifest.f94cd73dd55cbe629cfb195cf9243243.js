4c71bb3e9721c0a19af5d20ba190f1a6ebf91df2
Pankaj Kumar Sahu <pankajsahuhero@gmail.com>
2021-05-05 17:30:42 +0530

    Updates

---
+++ precache-manifest.f94cd73dd55cbe629cfb195cf9243243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63361d742be3036928df985cd2ab94e6",
+    "url": "/pankajsahu221/wedmeplz-task/index.html"
+  },
+  {
+    "revision": "ab2eaf6ee20efbe71314",
+    "url": "/pankajsahu221/wedmeplz-task/static/css/main.207800a9.chunk.css"
+  },
+  {
+    "revision": "e41feb6abb65a56bb9f9",
+    "url": "/pankajsahu221/wedmeplz-task/static/js/2.31d5abeb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pankajsahu221/wedmeplz-task/static/js/2.31d5abeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2eaf6ee20efbe71314",
+    "url": "/pankajsahu221/wedmeplz-task/static/js/main.b1f72be1.chunk.js"
+  },
+  {
+    "revision": "70b52b2f21325f624674",
+    "url": "/pankajsahu221/wedmeplz-task/static/js/runtime-main.faf90ce0.js"
+  }
+]);
